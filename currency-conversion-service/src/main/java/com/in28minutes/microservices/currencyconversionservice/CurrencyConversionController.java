0e83ec065f04debfe15c0a04098ea2afa3be8cb4
paul <paul.b.mills@gmail.com>
2019-05-28 19:23:47 +0100

    Creation of new service

---
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,21 @@
+package com.in28minutes.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+
+public class CurrencyConversionController {
+	
+	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrency(	@PathVariable String from , 
+											@PathVariable String to,
+											@PathVariable BigDecimal quantity)
+	{
+		return new CurrencyConversionBean(1L,  from, to, BigDecimal.ONE, quantity,quantity , 0);
+	}
+
+}
